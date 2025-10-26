a41961b41a56dabc765dcf841e0662fa270f4c00
cmurphyh <99055253+cmurphyh@users.noreply.github.com>
2025-10-26 11:49:45 -0700

    Figure S9 and S10 code

---
+++ Intertidal_Conc_Sig_Testing.R
@@ -0,0 +1,159 @@
+# Clear environment
+rm(list=ls()); graphics.off(); cat("\f")
+
+# Set working directory
+setwd("~/Desktop/Papers/Sediment Paper/Code/")
+
+# Load packages
+library(vegan)
+library(tidyverse)
+library(goeveg)
+library(ggplot2)
+library(dplyr)
+library(ggpubr)
+library(FSA)
+
+# Read dataset
+df <- read_csv('Intertidal_Sed_MP_SHP_v4.csv')
+
+# Transform 1st Order Concentration data into long format for ggplot
+df_1stOrder_Conc <- df %>%
+  select(Sample, Site, Location, Depth, Conc_NonFiber, Conc_Fiber) %>%
+  pivot_longer(cols = c( Conc_NonFiber, Conc_Fiber), 
+               names_to = "Particle_Type", values_to = "Concentration")
+
+#Add * to the UNB-2 samples that did not process below 500um
+df_1stOrder_Conc <- df_1stOrder_Conc %>%
+  mutate(mark_star = ifelse(
+    Location == "UNB-2" & Depth %in% c('10-15cm', '15-20cm', '20-25cm', '25-30cm', '30-35cm', '35-40cm', '40-45cm', '45-50cm', '50-55cm') & Particle_Type == "Conc_Fiber",
+    "*", ""
+  ))
+
+
+#################################################################################
+
+# Plot stacked bar chart for 1st Order Concentration
+
+# Define label replacements for Particle_Type
+conc_labels <- c(
+  Conc_NonFiber = "Non-Fiber",
+  Conc_Fiber = "Fiber"
+)
+
+#Plot
+p <- ggplot(df_1stOrder_Conc, aes(x = Depth, y = Concentration, fill = Particle_Type)) +
+  geom_bar(stat = "identity", position = "stack") +
+  geom_text(
+    aes(label = mark_star),
+    position = position_stack(vjust = 1.05),
+    size = 5
+  )+
+  facet_wrap(~ Location, scales = "free_y") +  # Create one panel per Location
+  #geom_vline(xintercept = vline_positions, color = "black", linetype = "solid", size = 0.5) +
+  theme_bw() +
+  labs(title = "Count-based MP Concentration: Intertidal Sediments",
+       x = NULL, y = expression(Concentration~"("*n^1~"MP"~g^{-1}~"Sediment dw"*")"),
+       fill = "Particle Type") +
+  scale_fill_manual(
+    values = c("steelblue", "darkorange"),
+    labels = conc_labels
+  ) +
+  #scale_y_continuous(limits = c(0, 20), expand = expansion(mult = c(0, 0.05))) +
+  theme(
+    text = element_text(family = "Times New Roman"),# global font
+    strip.text = element_text(family = "Times New Roman",     # facet labels
+                              size = 12, face = "bold"),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 10),
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 10),
+    axis.text.y = element_text(size = 12),
+    plot.title  = element_text(size = 12, face = "bold")
+  )
+#p
+
+p <- p + labs(
+  caption = "*indicates the 250-500 um sieved size class was not completed"
+) +
+  theme(
+    plot.caption = element_text(hjust = 0.5, size = 10, face = "italic")
+  )
+
+p
+
+#################################################################################
+################################################################################
+
+#Plot stacked bar charts for Morphological Composition
+
+
+# Transform All MP Composition data into long format for ggplot
+df_All_Comp <- df %>%
+  select(Sample, Site, Location, Depth, Perc_fiber, Perc_fiberbun, Perc_film, Perc_sphere, Perc_foam, Perc_fragment, Perc_pellet, Perc_TRWP) %>%
+  pivot_longer(cols = c(Perc_fiber, Perc_fiberbun, Perc_film, Perc_sphere, Perc_foam, Perc_fragment, Perc_pellet, Perc_TRWP), 
+               names_to = "Particle_Type", values_to = "Composition")
+
+#Add * to the UNB-2 samples that did not process below 500um
+df_All_Comp<- df_All_Comp %>%
+  mutate(mark_star = ifelse(
+    Location == "UNB-2" & Depth %in% c('10-15cm', '15-20cm', '20-25cm', '25-30cm', '30-35cm', '35-40cm', '40-45cm', '45-50cm', '50-55cm') & Particle_Type == "Perc_fiber",
+    "*", ""
+  ))
+
+# Define label replacements for Particle_Type
+comp_labels <- c(
+  Perc_fiber = "fiber",
+  Perc_fiberbun = "fiber bundle",
+  Perc_film = "film",
+  Perc_sphere = "sphere",
+  Perc_pellet = "pellet",
+  Perc_foam = "foam",
+  Perc_fragment = "fragment",
+  Perc_TRWP = "TRWP"
+)
+
+df_All_Comp <- df_All_Comp %>%
+  mutate(Composition = ifelse(is.na(Composition), 0, Composition))
+
+
+# Plot stacked bar chart for 1st Order Concentration
+p<- ggplot(df_All_Comp, aes(x = Depth, y = Composition * 100, fill = Particle_Type)) +
+  geom_bar(stat = "identity", position = "stack") +
+  geom_text(
+    aes(label = mark_star),
+    position = position_stack(vjust = 1.0),
+    size = 5
+  )+
+  facet_wrap(~ Location) +  # Create one panel per Location
+  theme_bw() +
+  labs(title = "Count-based Morphological MP Composition",
+       x = "Site", y = "Morphological Count Composition [%]",
+       fill = "Morphology") +
+  scale_fill_manual(
+    values = RColorBrewer::brewer.pal(n = 8, name = "Set2"),
+    labels = comp_labels
+  ) +
+  #scale_y_continuous(limits = c(0, 20), expand = expansion(mult = c(0, 0.05))) +
+  theme(
+    text = element_text(family = "Times New Roman"),# global font
+    strip.text = element_text(family = "Times New Roman",     # facet labels
+                              size = 12, face = "bold"),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 10),
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 10),
+    axis.text.y = element_text(size = 12),
+    plot.title  = element_text(size = 12, face = "bold")
+  )
+
+p
+
+p <- p + labs(
+  caption = "*indicates the 250-500 um sieved size class was not completed"
+) +
+  theme(
+    plot.caption = element_text(hjust = 0.5, size = 10, face = "italic")
+  )
+
+p
+
+
+
